6a5a3a257d0d10c93d200b698c490476ef5b403d
Lukas Kliukas <lukaskliukas@gmail.com>
2022-03-17 15:21:09 +0200

    Register FE console remove

--- public/js/Register.js
+++ public/js/Register.js
@@ -31,7 +31,6 @@ async function registerUser(registerUserData) {
 
 function handleErrors(erorrArray) {
   errorsContainerEl.innerHTML = '';
-  console.log('erorrArray ===', erorrArray);
   erorrArray.forEach((err) => {
     errorsContainerEl.innerHTML += `<p>${err.message}</p>`;
   });
