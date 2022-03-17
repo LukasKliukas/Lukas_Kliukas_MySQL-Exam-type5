18fb5d66908e0f7aaf5f3b2b0ef3fdb4534a7e3b
Lukas Kliukas <lukaskliukas@gmail.com>
2022-03-17 12:47:01 +0200

    Bills table fix FE

--- public/js/Bills.js
+++ public/js/Bills.js
@@ -1,1 +1,28 @@
+const BASE_URL = 'http://localhost:3000';
 const tableDiv = document.querySelector('.table');
+const billsEl = document.getElementById('bills');
+
+async function getBills() {
+  const token = localStorage.getItem('token24');
+  const resp = await fetch(`${BASE_URL}/bills/2`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const dataInJs = await resp.json();
+  console.log('dataInJs ===', dataInJs.data);
+  renderBills(dataInJs.data);
+}
+getBills();
+
+function renderBills(postsArray) {
+  postsArray.forEach((post) => {
+    const postTr = document.createElement('tr');
+    const postTr2 = document.createElement('tr');
+    const postId = document.createElement('td');
+    postId.textContent = `${post.id}`;
+    const postDescription = document.createElement('td');
+    postDescription.textContent = `${post.description}`;
+    const postAmount = document.createElement('td');
+    postAmount.textContent = `${post.amount}`;
+    billsEl.append(postTr, postId, postDescription, postAmount, postTr2);
+  });
+}
